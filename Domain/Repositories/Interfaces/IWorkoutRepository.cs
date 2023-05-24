fd040c2ab8404becbef2800a04775d61dd70b669
trueberryless <felix@schneider-it.at>
2023-05-24 11:42:08 +0200

    TODOs in code

--- Domain/Repositories/Interfaces/IWorkoutRepository.cs
+++ Domain/Repositories/Interfaces/IWorkoutRepository.cs
@@ -5,6 +5,8 @@ namespace Domain.Repositories.Interfaces;
 
 public interface IWorkoutRepository : IRepository<Workout>
 {
+    // TODO share workouts
+    
     Task<Workout> GetWorkoutById(int workoutId, CancellationToken ct = default);
     Task<List<Workout>> GetWorkoutsByUser(int userId, CancellationToken ct = default);
     Task<List<Workout>> GetWorkoutsByExercise(int exerciseId, CancellationToken ct = default);
